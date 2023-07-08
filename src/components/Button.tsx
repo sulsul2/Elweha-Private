48f5580287d86ce91f00591f45d6ba0eba8b4500
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-07-08 22:27:38 +0700

    feat: delete button varians

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ function Button({
 }: {
   text: string;
   type: "button" | "submit" | "reset" | undefined;
-  style: "primary" | "seccondary" | "third";
+  style: "primary" | "seccondary" | "third" | "delete";
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
   onSubmit?: FormEventHandler<HTMLButtonElement> | undefined;
 }) {
@@ -24,7 +24,9 @@ function Button({
             ? "bg-kOrange-400 text-white hover:bg-kOrange-300 active:bg-kOrange-500"
             : style == "seccondary"
             ? "bg-kOrange-200 text-kOrange-500 hover:bg-kOrange-300 active:bg-kOrange-400"
-            : "border border-kOrange-400 bg-white text-kOrange-400 hover:bg-kOrange-300 hover:text-white active:bg-kOrange-500 active:text-white"
+            : style == "third"
+            ? "border border-kOrange-400 bg-white text-kOrange-400 hover:bg-kOrange-300 hover:text-white active:bg-kOrange-500 active:text-white"
+            : "h-auto  w-auto bg-[#FC4C4C] px-5 py-3 text-white hover:bg-[#FF7C7C] active:bg-kRed"
         }`}
       >
         {text}
