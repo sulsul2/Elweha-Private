3f3acf47cc3330197ee8b14c6ee9c2b597c8ad7a
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-07-08 22:36:20 +0700

    fix: seccondary size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -23,7 +23,7 @@ function Button({
           style == "primary"
             ? "bg-kOrange-400 text-white hover:bg-kOrange-300 active:bg-kOrange-500"
             : style == "seccondary"
-            ? "bg-kOrange-200 text-kOrange-500 hover:bg-kOrange-300 active:bg-kOrange-400"
+            ? "h-auto w-auto bg-kOrange-200  px-5 py-3 text-kOrange-500 hover:bg-kOrange-300 active:bg-kOrange-400"
             : style == "third"
             ? "border border-kOrange-400 bg-white text-kOrange-400 hover:bg-kOrange-300 hover:text-white active:bg-kOrange-500 active:text-white"
             : "h-auto  w-auto bg-[#FC4C4C] px-5 py-3 text-white hover:bg-[#FF7C7C] active:bg-kRed"
