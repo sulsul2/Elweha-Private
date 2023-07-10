5ad83073480a9eb6982bcdc5de6597898743ff38
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-07-10 15:59:02 +0700

    refractor: package json, app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import DateFieldNormal from "./components/DateFieldNormal";
-import DateFieldRange from "./components/DateFieldRange";
-
 function App() {
-  return (
-  <>
-  <DateFieldNormal text="Pick a date"/>
-  </>);
+  return <></>;
 }
 
 export default App;
