62bc89432e758155230858bec622069fc1b4afca
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-07-12 08:32:59 +0700

    Update Table.tsx

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,28 +1,27 @@
-function Table({ data, column }: { data: any; column: any }) {
+function Table({ data, column}: { data: any; column: any}) {
   return (
     <>
-      <div className="flex h-auto w-full items-center justify-center">
-        <table className="flex h-[600px] w-[1200px] flex-col items-center justify-center">
+        <table className="flex h-[600px] w-[1200px] flex-col items-center overflow-y-visible">
           <thead>
             <tr>
               {column.map((row: any, idx: number) => {
                 return (
-                  <th key={idx} className="w-[200px] bg-[#FFD7BC] px-12 py-1 font-normal">
+                  <th key={idx} className = "h-auto w-[120px] bg-kOrange-100 py-1 font-normal">
                     {row}
                   </th>
                 );
               })}
             </tr>
           </thead>
-          <tbody className="flex flex-col items-center justify-center">
+          <tbody>
             {Object.values(data).map((obj: any, idx: number) => {
               return (
-                <tr key={idx} className="flex items-center justify-center">
+                <tr key={idx}>
                   {Object.values(obj).map((row: any, idx: number) => {
                     return (
                       <td
                         key={idx}
-                        className="flex w-[200px] items-center justify-center border-b-2 border-[#B8B5B5] py-3"
+                        className="h-auto w-[120px] text-center border-b-2 border-kGrey-100 py-3"
                       >
                         {row}
                       </td>
@@ -33,7 +32,6 @@ function Table({ data, column }: { data: any; column: any }) {
             })}
           </tbody>
         </table>
-      </div>
     </>
   );
 }
