3d96ac9c259898fc3881dfecf526d669548830ce
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-07-12 22:54:33 +0700

    fix: size, shadow

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -35,41 +35,39 @@ interface DropdownProps {
 
 const Dropdown = ({ placeholder, type, onChange, value }: DropdownProps) => {
   return (
-    <div className=" w-full rounded-lg bg-slate-700">
-      <Select
-        className="basic-single"
-        onChange={onChange}
-        placeholder={placeholder}
-        name={type}
-        options={options}
-        defaultValue={value == null ? null : { value: value, label: value }}
-        isSearchable={false}
-        theme={(theme) => ({
-          ...theme,
-          borderRadius: 0,
-          colors: {
-            ...theme.colors,
-            primary25: "#FD6701",
-            primary: "#A8A8A8",
-          },
-        })}
-        styles={{
-          control: (baseStyles) => ({
-            ...baseStyles,
-            borderRadius: "8px",
-            paddingLeft: "8px",
-            paddingTop: "2px",
-            paddingBottom: "2px",
-          }),
-          placeholder: (base) => ({
-            ...base,
-            fontSize: "16px",
-            color: "#A8A8A8",
-            fontWeight: 400,
-          }),
-        }}
-      />
-    </div>
+    <Select
+      className="basic-single shadow-button w-[180px] rounded-lg bg-slate-700"
+      onChange={onChange}
+      placeholder={placeholder}
+      name={type}
+      options={options}
+      defaultValue={value == null ? null : { value: value, label: value }}
+      isSearchable={false}
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          primary25: "#FD6701",
+          primary: "#A8A8A8",
+        },
+      })}
+      styles={{
+        control: (baseStyles) => ({
+          ...baseStyles,
+          borderRadius: "8px",
+          paddingLeft: "8px",
+          paddingTop: "2px",
+          paddingBottom: "2px",
+        }),
+        placeholder: (base) => ({
+          ...base,
+          fontSize: "16px",
+          color: "#A8A8A8",
+          fontWeight: 400,
+        }),
+      }}
+    />
   );
 };
 
