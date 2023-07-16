e0515a826146c82a8d824ec7b889c5d39b0b384a
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-07-16 15:16:33 +0700

    feat: Format Rupiah

--- src/pages/PajakPerusahaan/CardPajakPerusahaan.tsx
+++ src/pages/PajakPerusahaan/CardPajakPerusahaan.tsx
@@ -1,27 +1,24 @@
+import { FormatRupiah } from "@arismun/format-rupiah";
 import Checkbox from "../../components/Checkbox";
 
-const CardPajakPerusahaan = () => {
+function CardPajakPerusahaan({
+  label,
+  value,
+}: {
+  label: string;
+  value: number;
+}) {
   return (
-    <div className=" mt-5 rounded-[10px] bg-white p-[30px] drop-shadow-card">
-      <div className=" md:flex md:justify-between ">
-        <p className="text-20 font-bold">Total Pendapatan</p>
-        <p className="text-20">Rp100.000.0000.000,-</p>
-      </div>
-      <hr className=" mb-4 mt-1 h-[2px] bg-kGrey-100" />
-      <div className=" flex justify-between">
-        <Checkbox type="check" id="" label="Jenis 1" />
-        <p>Rp100.000.0000.000,-</p>
-      </div>
-      <div className="mt-4 flex justify-between gap-1">
-        <Checkbox type="check" id="" label="Jenis 2" />
-        <p>Rp100.000.0000.000,-</p>
-      </div>
+    <>
       <div className="mt-4 flex w-full justify-between gap-1">
-        <Checkbox type="check" id="" label="Jenis Lain Kalo ada" />
-        <p className=" break-words">Rp100.000.0000.000,-</p>
+        <Checkbox type="check" id="" label={label} />
+        <span>
+          <FormatRupiah value={value} />
+          ,-
+        </span>
       </div>
-    </div>
+    </>
   );
-};
+}
 
 export default CardPajakPerusahaan;
--- src/pages/PajakPerusahaan/PajakPerusahaan.tsx
+++ src/pages/PajakPerusahaan/PajakPerusahaan.tsx
@@ -2,6 +2,23 @@ import { useState } from "react";
 import Dropdown from "../../components/Dropdown";
 import Navbar from "../../components/Navbar";
 import CardPajakPerusahaan from "./CardPajakPerusahaan";
+import { FormatRupiah } from "@arismun/format-rupiah";
+
+function KoreksiCard({ value }: { value: number }) {
+  return (
+    <div className="mb-4 flex justify-between xl:gap-5">
+      <p className=" w-2/5 xl:w-full">Penyusutan Kendaraan</p>
+      <div className=" flex w-3/5 justify-end gap-3">
+        <span className=" w-3/5 break-words text-end lg:w-full xl:w-full">
+          <FormatRupiah value={value} />
+          ,-
+        </span>
+        <div className=" h-6 w-6 bg-slate-700"></div>
+        <div className=" h-6 w-6 bg-slate-700"></div>
+      </div>
+    </div>
+  );
+}
 
 function PajakPerusahaan() {
   const [month, setMonth] = useState("Januari 2023");
@@ -25,34 +42,66 @@ function PajakPerusahaan() {
           </div>
         </div>
         <div className="">
-          <div className="grid-cols-2 gap-12 xl:grid">
-            <CardPajakPerusahaan />
-            <CardPajakPerusahaan />
+          <div className="grid-cols-2 gap-x-12 xl:grid">
+            <div className=" mt-5 rounded-[10px] bg-white p-[30px] drop-shadow-card">
+              <div className=" md:flex md:justify-between ">
+                <p className="text-20 font-bold">Total Pendapatan</p>
+                <span className="text-20">
+                  <FormatRupiah value={100000000000} />
+                  ,-
+                </span>
+              </div>
+
+              <hr className=" mb-4 mt-1 h-[2px] bg-kGrey-100" />
+              <CardPajakPerusahaan
+                label={"Penyusutan Kendaraan"}
+                value={100000000000}
+              />
+              <CardPajakPerusahaan
+                label={"Penyusutan kontol"}
+                value={100000000000}
+              />
+            </div>
+            <div className=" mt-5 rounded-[10px] bg-white p-[30px] drop-shadow-card">
+              <div className=" md:flex md:justify-between ">
+                <p className="text-20 font-bold">Total Pengeluaran</p>
+                <span className="text-20">
+                  <FormatRupiah value={100000000000} />
+                  ,-
+                </span>
+              </div>
+
+              <hr className=" mb-4 mt-1 h-[2px] bg-kGrey-100" />
+              <CardPajakPerusahaan
+                label={"Penyusutan Kendaraan"}
+                value={100000000000}
+              />
+              <CardPajakPerusahaan
+                label={"Penyusutan kontol"}
+                value={100000000000}
+              />
+            </div>
           </div>
           <div className=" mt-3 w-full rounded-[10px] bg-white p-[30px] drop-shadow-card xl:mx-auto xl:w-fit">
             <div className=" xl:flex xl:items-center xl:gap-9">
               <p className=" text-20 font-bold">Laba Sebelum Pajak</p>
-              <p className=" text-20">Rp100.000.000.000,-</p>
+              <span className="text-20">
+                <FormatRupiah value={100000000000} />
+                ,-
+              </span>
             </div>
             <div className=" mt-[25px]">
               <div className=" gap-10 xl:flex xl:justify-between">
                 <div className="">
-                  <div className="flex items-center justify-between">
+                  <div className="flex items-center justify-between gap-3">
                     <p className=" text-20 font-bold">Koreksi Positif</p>
                     <p className=" text-16 font-light text-kOrange-400">
                       Tambah Koreksi
                     </p>
                   </div>
-                  <div className="flex justify-between xl:gap-5">
-                    <p className=" w-2/5 xl:w-full">Penyusutan Kendaraan</p>
-                    <div className=" flex w-3/5 justify-end ">
-                      <p className=" w-3/5 break-words text-end lg:w-full xl:w-full">
-                        Rp100.000.000.000.000,-
-                      </p>
-                      <img src="" alt="" />
-                      <img src="" alt="" />
-                    </div>
-                  </div>
+                  <KoreksiCard value={100000000000} />
+                  <KoreksiCard value={100000000000} />
+                  <KoreksiCard value={100000000000} />
                 </div>
                 <div className="">
                   <div className="mt-6 flex items-center justify-between xl:mt-0">
@@ -61,25 +110,22 @@ function PajakPerusahaan() {
                       Tambah Koreksi
                     </p>
                   </div>
-                  <div className="flex justify-between xl:gap-5">
-                    <p className=" w-2/5 xl:w-full ">Penyusutan Kendaraan</p>
-                    <div className=" flex w-3/5 justify-end">
-                      <p className=" w-3/5 break-words text-end lg:w-full">
-                        Rp100.000.000.000.000,-
-                      </p>
-                      <img src="" alt="" />
-                      <img src="" alt="" />
-                    </div>
-                  </div>
+                  <KoreksiCard value={100000000} />
                 </div>
               </div>
               <div className=" mt-6">
                 <p className="text-20 font-bold">Laba Bersih Fiskal</p>
-                <p className=" text-20">Rp100.000.000.000.000,-</p>
+                <span className="text-20">
+                  <FormatRupiah value={100000000000} />
+                  ,-
+                </span>
               </div>
               <div className=" mt-6">
                 <p className="text-20 font-bold">Pembulatan</p>
-                <p className="text-20">Rp100.000.000.000.000,-</p>
+                <span className="text-20">
+                  <FormatRupiah value={100000000000} />
+                  ,-
+                </span>
               </div>
             </div>
           </div>
