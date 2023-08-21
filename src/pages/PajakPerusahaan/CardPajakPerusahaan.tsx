44e8a0bee5761a83a00d395d5a9bad27dac994e1
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-08-22 00:28:41 +0700

    kugeru

--- src/pages/PajakPerusahaan/CardPajakPerusahaan.tsx
+++ src/pages/PajakPerusahaan/CardPajakPerusahaan.tsx
@@ -4,16 +4,21 @@ import Checkbox from "../../components/Checkbox";
 function CardPajakPerusahaan({
   label,
   value,
+  isChecked,
+  onClick,
 }: {
   label: string;
   value: number;
+  isChecked: boolean;
+  onClick : React.MouseEventHandler<HTMLDivElement> | undefined;
 }) {
+
   return (
     <>
-      <div className="mt-4 flex w-full flex-col items-start justify-between md:flex-row">
+      <div onClick={onClick} className="mt-4 flex w-full flex-col items-start justify-between md:flex-row">
         <div className=" shrink-0">
           {" "}
-          <Checkbox type="check" id="" label={label} checked = {value == 0 ? false : true} />
+          <Checkbox onChange={()=>null} type="check" id="" label={label} checked = {value == 0 ? false : isChecked} />
         </div>
         <span className=" break-words text-end lg:w-full xl:w-full">
           <FormatRupiah value={value} />
--- src/pages/PajakPerusahaan/PajakPerusahaan.tsx
+++ src/pages/PajakPerusahaan/PajakPerusahaan.tsx
@@ -31,6 +31,11 @@ function PajakPerusahaan() {
   const [totalKoreksiNegatif, setTotalKoreksiNegatif] = useState(0);
   const [totalPendapatan, setTotalPendapatan] = useState(0);
   const [totalPengeluaran, setTotalPengeluaran] = useState(0);
+  const [totalDummyPendapatan, setTotalDummyPendapatan] = useState(0);
+  const [totalDummyPengeluaran, setTotalDummyPengeluaran] = useState(0);
+  
+  const [onSelectedPendapatan, setOnSelectedPendapatan] = useState<Array<string>>([]);
+  const [onSelectedPengeluaran, setOnSelectedPengeluaran] = useState<Array<string>>([]);
 
   const [kategoriPendapatan, setKategoriPendapatan] = useState<
     Array<{ value: number; label: string }>
@@ -57,9 +62,9 @@ function PajakPerusahaan() {
     const laba =
       1000 *
       Math.floor(
-        (totalPendapatan -
+        (totalDummyPendapatan -
           totalKoreksiNegatif +
-          (totalKoreksiPositif - totalPengeluaran)) /
+          (totalKoreksiPositif - totalDummyPengeluaran)) /
           1000
       );
 
@@ -134,21 +139,44 @@ function PajakPerusahaan() {
     });
 
     setKategoriPendapatan(updatedKategoriPendapatan);
+    // updatedKategoriPendapatan.map((row:any) => {
+    //   console.log(row.value > 0);
+    //   if (row.value > 0){
+    //     setOnSelectedPendapatan([...onSelectedPendapatan, row.label as string])
+    //   } 
+    //   else {
+    //     setOnSelectedPendapatan([...onSelectedPendapatan])
+    //   }
+    // })
+    // console.log(onSelectedPendapatan);
+    setOnSelectedPendapatan(prevSelected => {
+      const updatedSelected = updatedKategoriPendapatan
+        .filter(row => row.value > 0)
+        .map(row => row.label);
+      return updatedSelected;
+    });
   };
-
+  
   const pengeluaranPerKategori = () => {
     const updatedKategoriPengeluaran = kategoriPengeluaran.map((row: any) => {
       const accumulatedValue = dataPengeluaran.reduce(
         (total, row2: any) =>
-          row2.kategori === row.label ? total + row2.jumlah : total,
+        row2.kategori === row.label ? total + row2.jumlah : total,
         0
-      );
-      return { ...row, value: accumulatedValue };
-    });
-    console.log(updatedKategoriPengeluaran);
-    setKategoriPengeluaran(updatedKategoriPengeluaran);
-  };
+        );
+        return { ...row, value: accumulatedValue };
+      });
+      // console.log(updatedKategoriPengeluaran);
+      setKategoriPengeluaran(updatedKategoriPengeluaran);
 
+      setOnSelectedPengeluaran(prevSelected => {
+        const updatedSelected = updatedKategoriPengeluaran
+          .filter(row => row.value > 0)
+          .map(row => row.label);
+        return updatedSelected;
+      });
+    };
+    
   const getKoreksi = async (sifat: string) => {
     setIsLoadKoreksi(true);
     if (token) {
@@ -197,10 +225,11 @@ function PajakPerusahaan() {
         const kategori = await getWithAuth(token, "kategori-pendapatan");
         setKategoriPendapatan(
           kategori.data.data.map((data: any) => {
-            return { value: 0, label: data.nama };
+            return { value:0 , label: data.nama };
           })
         );
         setTotalPendapatan(pendapatan.data.data.total_pendapatan);
+        setTotalDummyPendapatan(pendapatan.data.data.total_pendapatan);
       } catch (error) {
         toastError("Get Some Data Failed");
       } finally {
@@ -239,6 +268,7 @@ function PajakPerusahaan() {
           })
         );
         setTotalPengeluaran(pengeluaran.data.data.total_pengeluaran);
+        setTotalDummyPengeluaran(pengeluaran.data.data.total_pengeluaran);
       } catch (error) {
         toastError("Get Some Data Failed");
       } finally {
@@ -332,6 +362,31 @@ function PajakPerusahaan() {
     }
   };
 
+  const handleCheck = (label: string, isChecked:boolean, value:number, tipe: string) => {
+    var temppendapatan = totalDummyPendapatan
+    var temppengeluaran = totalDummyPengeluaran
+    if(tipe=="pendapatan"){
+      if (!isChecked) {
+        setOnSelectedPendapatan([...onSelectedPendapatan, label]);
+        setTotalDummyPendapatan(temppendapatan + value)
+      } else {
+        setOnSelectedPendapatan(onSelectedPendapatan.filter((item) => item !== label));
+        setTotalDummyPendapatan(temppendapatan - value)
+      }
+      // console.log(onSelectedPendapatan)
+    } else {
+      if (!isChecked) {
+        setOnSelectedPengeluaran([...onSelectedPengeluaran, label]);
+        setTotalDummyPengeluaran(temppengeluaran + value)
+      } else {
+        setOnSelectedPengeluaran(onSelectedPengeluaran.filter((item) => item !== label));
+        setTotalDummyPengeluaran(temppengeluaran - value)
+      }
+      // console.log(onSelectedPendapatan)
+
+    }
+  };
+
   useEffect(() => {
     getTotalPendapatan();
     getTotalPengeluaran();
@@ -339,7 +394,7 @@ function PajakPerusahaan() {
 
   useEffect(() => {
     countPph();
-  }, [totalPendapatan, totalPengeluaran]);
+  }, [totalDummyPendapatan, totalDummyPengeluaran]);
 
   useEffect(() => {
     pendapatanPerKategori();
@@ -530,35 +585,37 @@ function PajakPerusahaan() {
             <div className=" md:flex md:justify-between ">
               <p className="text-20 font-bold">Total Pendapatan</p>
               <span className="text-20">
-                <FormatRupiah value={totalPendapatan} />
+                <FormatRupiah value={totalDummyPendapatan} />
                 ,-
               </span>
             </div>
 
             <hr className=" mb-4 mt-1 h-[2px] bg-kGrey-100" />
-            {kategoriPendapatan.map((row: { value: number; label: string }) =>
-              isLoadPendapatan ? (
-                <Load />
-              ) : (
-                <CardPajakPerusahaan label={row.label} value={row.value} />
-              )
+            {kategoriPendapatan.map((row: { value: number; label: string }) => {
+              var isChecked = onSelectedPendapatan.includes(row.label)
+              if (isLoadPendapatan) {
+                return <Load />;
+              }
+              return <CardPajakPerusahaan label={row.label} value={row.value} isChecked={isChecked} onClick={() => handleCheck(row.label, isChecked, row.value, "pendapatan")} />
+            }
             )}
           </div>
           <div className="rounded-[10px] bg-white p-[30px] drop-shadow-card">
             <div className=" md:flex md:justify-between ">
               <p className="text-20 font-bold">Total Pengeluaran</p>
               <span className="text-20">
-                <FormatRupiah value={totalPengeluaran} />
+                <FormatRupiah value={totalDummyPengeluaran} />
                 ,-
               </span>
             </div>
             <hr className=" mb-4 mt-1 h-[2px] bg-kGrey-100" />
-            {kategoriPengeluaran.map((row: { value: number; label: string }) =>
-              isLoadPengeluaran ? (
-                <Load />
-              ) : (
-                <CardPajakPerusahaan label={row.label} value={row.value} />
-              )
+            {kategoriPengeluaran.map((row: { value: number; label: string }) => {
+              var isChecked = onSelectedPengeluaran.includes(row.label)
+              if (isLoadPengeluaran) {
+                return <Load />;
+              }
+              return <CardPajakPerusahaan label={row.label} value={row.value} isChecked={isChecked} onClick={() => handleCheck(row.label, isChecked, row.value, "pengeluaran")} />
+            }
             )}
           </div>
         </div>
@@ -566,7 +623,7 @@ function PajakPerusahaan() {
           <div className=" xl:flex xl:items-center xl:gap-9">
             <p className=" text-20 font-bold">Laba Sebelum Pajak</p>
             <span className="text-20">
-              <FormatRupiah value={totalPendapatan - totalPengeluaran} />
+              <FormatRupiah value={totalDummyPendapatan - totalDummyPengeluaran} />
               ,-
             </span>
           </div>
@@ -612,9 +669,9 @@ function PajakPerusahaan() {
               <span className="text-20">
                 <FormatRupiah
                   value={
-                    totalPendapatan -
+                    totalDummyPendapatan -
                     totalKoreksiNegatif +
-                    (totalKoreksiPositif - totalPengeluaran)
+                    (totalKoreksiPositif - totalDummyPengeluaran)
                   }
                 />
                 ,-
@@ -627,9 +684,9 @@ function PajakPerusahaan() {
                   value={
                     1000 *
                     Math.floor(
-                      (totalPendapatan -
+                      (totalDummyPendapatan -
                         totalKoreksiNegatif +
-                        (totalKoreksiPositif - totalPengeluaran)) /
+                        (totalKoreksiPositif - totalDummyPengeluaran)) /
                         1000
                     )
                   }
